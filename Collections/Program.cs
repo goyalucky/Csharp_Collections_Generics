96e5ae111c89f887dbd2fc03fa8b86ac19067281
goyallucky <luckygoyal221982@gmail.com>
2026-01-16 15:18:11 +0530

    [Lucky Goyal] Add. program.cs file

---
+++ Collections/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Collections;
+using System;
+using Set;
+using Queue;
+using Map;
+using List;
+class Program
+{
+    static void Main(string[] args)
+    {
+        SetMain.Run();
+        // QueueMain.Run();
+        // MapMain.Run();
+        // ListMain.Run();
+
+        Console.WriteLine("Hello, World!");
+        Console.ReadLine();
+    }
+}
