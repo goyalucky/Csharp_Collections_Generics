3a79a4da9cd4953330d1ac8f75115c291263dcaa
goyallucky <luckygoyal221982@gmail.com>
2026-01-17 15:27:04 +0530

    [Lucky Goyal] Add Program.cs file to run all the programs

---
+++ CollectionExtra/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace CollectionExtra;
+using System;
+using StudentPerformanceAnalyzer;
+using SmartShoppingCart;
+using EmployeeLeaveSystem;
+using BankFraudDetectionSystem;
+using CabAssignmentSystem;
+using ExamNormalizationSystem;
+using ChatCleanupSystem;
+using LibraryFineSystem;
+using InventoryPredictionSystem;
+using HospitalTriageSystem;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // StudentMain.Start();
+        // ShoppingMain.Start();
+        // LeaveMain.Start();
+        // BankFraudMain.Start();
+        // CabAssignmentMain.Start();
+        // ExamMain.Start();
+        // ChatMain.Start();
+        // LibraryFineMain.Start();
+        // InventoryMain.Start();
+        HospitalMain.Start();
+    }
+}
